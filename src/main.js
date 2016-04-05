1f823e7a9acef61c32bae349982e3291dca14118
baloran <contact@baloran.fr>
2016-04-05 15:59:38 +0200

    add hack font

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import Console from './components/Console.vue'
 import Contact from './components/Contact.vue'
+import Home from './components/Home.vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
@@ -10,6 +11,10 @@ Vue.use(require('vue-resource'))
 let router = new VueRouter()
 
 router.map({
+  '/': {
+    name: 'home',
+    component: Home
+  },
   '/m/:slug': {
     name: 'm',
     component: Console
