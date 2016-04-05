e2bd339b85ace444f0b9f15c6de156335aa5333b
baloran <contact@baloran.fr>
2016-04-05 16:01:21 +0200

    add home componenent

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <p>I love making, try things and innovate. Currently I try with my friends at <a href="https://wino.fr">wino</a> to help you to choose the good wines.</p>
+    <p>Looking for an intership for make great things with you.</p>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="stylus" scoped>
+
+.home
+  width 600px
+  height 100%
+  margin 0 auto
+  margin-top -140px
+  display flex
+  flex-direction column
+  justify-content center
+  p
+    margin-bottom 20px
+    line-height 18px
+    font-size 14px
+
+</style>
